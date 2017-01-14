ce6e6563b8a7c1805c2a2fe7990043a44831fec3
kmrgithub <kmreilly.ny@gmail.com>
2017-01-14 12:31:47 -0500

    Merged Bryce's robot code

--- src/org/usfirst/frc/team3950/robot/RobotMap.java
+++ src/org/usfirst/frc/team3950/robot/RobotMap.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team3950.robot;
 
+//import com.kauailabs.navx.frc.*;
+import edu.wpi.first.wpilibj.*;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -7,13 +10,16 @@ package org.usfirst.frc.team3950.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    public static TalonSRX leftFrontDriveMotor = new TalonSRX(0);
+    public static TalonSRX leftBackDriveMotor = new TalonSRX(1);
+    public static TalonSRX rightFrontDriveMotor = new TalonSRX(2);
+    public static TalonSRX rightBackDriveMotor = new TalonSRX(3);
+    public static Solenoid driveGearShiftSolenoid = new Solenoid(0);
+   // public static AHRS ahrs = new AHRS(SerialPort.Port.kMXP);
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
 }
--- src/org/usfirst/frc/team3950/robot/subsystems/DrivetrainSubsystem.java
+++ src/org/usfirst/frc/team3950/robot/subsystems/DrivetrainSubsystem.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team3950.robot.subsystems;
 
 import org.usfirst.frc.team3950.robot.RobotMap;
 import org.usfirst.frc.team3950.robot.commands.*;
-import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -13,10 +13,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *Attire!
  */
 public class DrivetrainSubsystem extends Subsystem {
-    CANTalon leftFront;
-    CANTalon leftBack;
-    CANTalon rightFront;
-    CANTalon rightBack;
+    TalonSRX leftFront;
+    TalonSRX leftBack;
+    TalonSRX rightFront;
+    TalonSRX rightBack;
     RobotDrive drivetrain;
     Solenoid gearShiftSolenoid;
     // Put methods for controlling this subsystem
