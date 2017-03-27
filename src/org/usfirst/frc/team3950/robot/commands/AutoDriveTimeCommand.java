fce9571e36b303b62bc190abee1c0068cf9faeab
RoboGym <RoboGym@NorthShore.com>
2017-03-27 19:22:12 -0400

    AutoDriveTimeCommand now has left and right gear autos.

--- src/org/usfirst/frc/team3950/robot/commands/AutoDriveTimeCommand.java
+++ src/org/usfirst/frc/team3950/robot/commands/AutoDriveTimeCommand.java
@@ -1,7 +1,12 @@
 package org.usfirst.frc.team3950.robot.commands;
 
 import org.usfirst.frc.team3950.robot.Robot;
+import org.usfirst.frc.team3950.robot.RobotLogger;
+import org.usfirst.frc.team3950.robot.subsystems.AxisCameraSubsystem;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -9,10 +14,12 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class AutoDriveTimeCommand extends Command {
 	
-	private double currTime;
-	private double startTime;
-	private double driveTime = 1900;
-	private double pauseTime = 100;
+	private static RobotLogger logger = new RobotLogger(AutoDriveTimeCommand.class);
+	private long currTime;
+	private long startTime;
+	private long driveTime = 1900;
+	private long pauseTime = 500;
+	private long subTime;
 
     public AutoDriveTimeCommand() {
         // Use requires() here to declare subsystem dependencies
@@ -22,27 +29,88 @@ public class AutoDriveTimeCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        isFinished = false;
     	startTime = System.currentTimeMillis();
+    	logger.log(RobotLogger.LoggerLevel.debug, "startTime is:" + startTime);
     }
 
+    boolean loggerBool = false;
+    boolean isFinished = false;
+    
+    boolean shouldITurn = true;
+    long forward1Time = 3800;
+    long forward2Time = 0;
+    long turnTime = 0;
+    long reverseTime = 3800;
+    double turnAngle = 0;
+    
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	currTime = System.currentTimeMillis();
-    	if(currTime - startTime <= driveTime){
-    		Robot.drivetrainSubsystem.Drive(-.75, 0.03);
+    	logger.log(RobotLogger.LoggerLevel.debug, "currTime is:" + currTime);
+
+    	if((currTime - startTime) <= forward1Time) {
+    		// set voltage for robotdrive
+    		Robot.drivetrainSubsystem.Drive(-.75, 0); //0.03);
+    	} else {
+    		if(shouldITurn) {
+    			shouldITurn = false;
+    			// turn the robot
+    			if(turnAngle != 0) {
+    				// then turn the robot
+    	    		Robot.drivetrainSubsystem.Drive(0, turnAngle / 90.0); //0.03);
+    			}
+    			turnTime = System.currentTimeMillis() - currTime;
+    		}
+   	    	if((currTime - startTime) <= (forward1Time + forward2Time + turnTime)) {
+   	    		// set voltage for robotdrive
+   	    		Robot.drivetrainSubsystem.Drive(-.75, 0); //0.03);
+   	    	} else {
+   	   	    	if((currTime - startTime) <= (forward1Time + forward2Time + turnTime + reverseTime)) {
+   	   	    		// set voltage for robotdrive
+   	   	    		Robot.drivetrainSubsystem.Drive(.75, 0); //0.03);
+   	   	    	} else {
+   	        		Robot.drivetrainSubsystem.Drive(0, 0); //0.03);
+   	        		isFinished = true;
+   	   	    	}
+   	    	}
     	}
-    	else if(currTime - startTime > driveTime && currTime - driveTime < pauseTime) {
+    	
+/*    	if((currTime - startTime) <= driveTime) {
+    		Robot.drivetrainSubsystem.Drive(-.75, 0); //0.03);
+    	} else {
+    		if(loggerBool == false) {
+    			loggerBool = true;
+    			logger.log(RobotLogger.LoggerLevel.debug, "PauseTime - currTime - startTime is:" + (currTime - startTime));
+    		}
+    		Robot.drivetrainSubsystem.Drive(.75, 0); //0.03);
+    		if((currTime - startTime) > (driveTime + pauseTime)) {
+        		logger.log(RobotLogger.LoggerLevel.debug, "currTime - startTime is:" + (currTime - startTime));
+        		Robot.drivetrainSubsystem.Drive(0, 0); //0.03);
+        		isFinished = true;
+    		}
+    	} */
+/*    	
+    	else if((currTime - startTime) == driveTime) {
+			subTime = System.currentTimeMillis();
+			
+		}
+    	
+    	else if(currTime - startTime >= driveTime) {
+    		logger.log(RobotLogger.LoggerLevel.debug, "currTime - startTime is:" + (currTime - startTime));
+    		logger.log(RobotLogger.LoggerLevel.debug, "currTime - driveTime" + (currTime - driveTime));
     		Robot.drivetrainSubsystem.Drive(.75, 0);
     	}
-    	else if(currTime - driveTime >= pauseTime) {
+
+    	else if(currTime - subTime >= pauseTime) {
+    		logger.log(RobotLogger.LoggerLevel.debug, "currTime - subTime" + (currTime - driveTime));
     		Robot.drivetrainSubsystem.Drive(0, 0);
-    		
-    	}
+    	} */
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isFinished;
     }
 
     // Called once after isFinished returns true
